6eb31f869833dd70d2c43789d901009ee1596a15
contraflux <eg.rosenfeld@icloud.com>
2026-02-13 22:46:33 -0500

    Collapsing navbar

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const content = document.getElementById("content");
 const navbar = document.getElementById("navbar")
+const navbar_toggle = document.getElementById("navbar-toggle");
+const navbar_toggle_1 = document.getElementById("navbar-toggle-1");
+const navbar_toggle_2 = document.getElementById("navbar-toggle-2");
+const navbar_toggle_3 = document.getElementById("navbar-toggle-3");
+const navbar_dropdown = document.getElementById("navbar-dropdown");
+let navbar_state = false;
 
+// Setup
+navbar_dropdown.style.top = -navbar_dropdown.offsetHeight + 75 + "px";
+
+// Event listeneers
 window.addEventListener("DOMContentLoaded", () => {
     content.style.opacity = 1;
 });
@@ -11,4 +21,31 @@ document.addEventListener("wheel", (e) => {
     } else {
         navbar.style.position = "fixed";
     }
+    hideNavbarDropdown();
 })
+
+navbar_toggle.addEventListener("click", (e) => {
+    if (navbar_state) {
+        hideNavbarDropdown();
+    } else {
+        showNavbarDropdown();
+    }
+});
+
+function showNavbarDropdown() {
+    navbar_dropdown.style.opacity = 1;
+    navbar_dropdown.style.top = 75 + "px";
+    navbar_state = true;
+    navbar_toggle_1.style.transform = "translate(0, 11px) rotate(45deg)";
+    navbar_toggle_2.style.opacity = 0;
+    navbar_toggle_3.style.transform = "translate(0, -11px) rotate(-45deg)";
+}
+
+function hideNavbarDropdown() {
+    navbar_dropdown.style.opacity = 1;
+    navbar_dropdown.style.top = -navbar_dropdown.offsetHeight + 75 + "px";
+    navbar_state = false;
+    navbar_toggle_1.style.transform = "translate(0, 0) rotate(0deg)";
+    navbar_toggle_2.style.opacity = 1;
+    navbar_toggle_3.style.transform = "translate(0, 0) rotate(0deg)";
+}
